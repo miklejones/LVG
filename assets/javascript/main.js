30266347436edebc51011d47a91773117e8187b5
Leo Orellana <leoorellana@lawn-143-215-61-9.lawn.gatech.edu>
2018-04-05 18:49:28 -0400

    google places api add

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,52 +1,51 @@
+var map;
+var service;
+var infowindow;
+var pyrmont;
+
+function initialize(lat, long) {
+    pyrmont = new google.maps.LatLng(lat, long);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+    });
+
+}
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            console.log(place);
+        }
+    }
+}
+
+function onPositionReceived(position) {
+    console.log(position);
+
+    initialize(position.coords.latitude, position.coords.longitude);
+
+    var request = {
+        location: pyrmont,
+        radius: '500',
+        type: ['restaurant']
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+}
+
+function locationNotReceived(positionError) {
+    console.log(positionError);
+}
+
 $(document).ready(function () {
-
-    var loc = document.getElementById("myloc");
-
-    function onPositionReceived(position) {
-        console.log(position);
-    }
-        function locationNotReceived(positionError) {
-        console.log(positionError);
-    }
-    if (navigator.geolocation){
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(onPositionReceived, locationNotReceived);
-
     }
-
-
 });
 
-// Adding click event listen listener to all buttons
-
-    // event.preventDefault() can be used to prevent an event's default behavior.
-    // Here, it prevents the submit button from trying to submit a form when clicked
-    // event.preventDefault();
-
-    // geolocation
-    // var queryURLgeo = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDHegTvoYcDXW5dGr4-Nlq2Ocuhq733V_I";
-
-    // queryURL is the url we'll use to query the API
 
 
-    // yelp api key
-
-    // var queryURL = "https://maps.googleapis.com/maps/api/js?AIzaSyAS6rcW7TumaYUczp3JckeTH46aA7D2WyU";
-
-    // open weather key
-    // queryURLweather +="https://api.openweathermap.org/data/2.5/weather?q" + queryURLgeo + "&appid=0376d3f7071bd7a61aec6379bed27dbc";
-
-    // Performing an AJAX request with the queryURL
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     $(".container").text(JSON.stringify(response));
-    //     console.log(response);
-    //
-    //     var results = response.data;
-    //     console.log(results);
-    //
-    // })
-//
-//
-// });
